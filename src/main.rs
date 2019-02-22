3823aeee6bfadbcc2d6fd2cf99b247c20676cd8f
rschwalk <rschwalk@outlook.com>
2019-02-22 20:41:55 +0100

    Button test

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ extern crate relm;
 #[macro_use]
 extern crate relm_derive;
 
-use relm::{Relm, Update, Widget};
 use gtk::prelude::*;
-use gtk::{Window, Inhibit, WindowType};
+use gtk::Orientation::Vertical;
+use gtk::{Button, Inhibit, Label, Window, WindowType};
+use relm::{Relm, Update, Widget};
 
 struct Model {
     // …
@@ -18,10 +19,18 @@ enum Msg {
     Quit,
 }
 
+#[derive(Clone)]
+struct Widgets {
+    counter_label: Label,
+    minus_buttton: Button,
+    plus_button: Button,
+    window: Window,
+}
+
 struct Win {
     // …
     model: Model,
-    window: Window,
+    widgets: Widgets,
 }
 
 impl Update for Win {
@@ -34,8 +43,7 @@ impl Update for Win {
 
     // Return the initial model.
     fn model(_: &Relm<Self>, _: ()) -> Model {
-        Model {
-        }
+        Model {}
     }
 
     // The model may be updated when a message is received.
@@ -53,16 +61,33 @@ impl Widget for Win {
 
     // Return the root widget.
     fn root(&self) -> Self::Root {
-        self.window.clone()
+        self.widgets.window.clone()
     }
 
     // Create the widgets.
     fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        let vbox = gtk::Box::new(Vertical, 0);
+
+        let plus_button = Button::new_with_label("+");
+        vbox.add(&plus_button);
+
+        let counter_label = Label::new("0");
+        vbox.add(&counter_label);
+
+        let minus_buttton = Button::new_with_label("-");
+        vbox.add(&minus_buttton);
+
         // GTK+ widgets are used normally within a `Widget`.
         let window = Window::new(WindowType::Toplevel);
+        window.add(&vbox);
 
         // Connect the signal `delete_event` to send the `Quit` message.
-        connect!(relm, window, connect_delete_event(_, _), return (Some(Msg::Quit), Inhibit(false)));
+        connect!(
+            relm,
+            window,
+            connect_delete_event(_, _),
+            return (Some(Msg::Quit), Inhibit(false))
+        );
         // There is also a `connect!()` macro for GTK+ events that do not need a
         // value to be returned in the callback.
 
@@ -70,7 +95,12 @@ impl Widget for Win {
 
         Win {
             model,
-            window: window,
+            widgets: Widgets {
+                counter_label,
+                minus_buttton,
+                plus_button,
+                window: window,
+            },
         }
     }
 }
