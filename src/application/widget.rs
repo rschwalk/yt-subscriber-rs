77b203bbdddde1aaf658d34b4e3499c1b7f71c77
rschwalk <rschwalk@outlook.com>
2019-03-03 20:50:22 +0100

    switched to glade

--- src/application/widget.rs
+++ src/application/widget.rs
@@ -28,12 +28,16 @@ pub struct MainWindow {
 
 impl MainWindow {
     pub fn build_ui(application: &gtk::Application) -> Self {
-        let header = gtk::HeaderBar::new();
-        header.set_show_close_button(true);
+        let glade_src = include_str!("ui/main.glade");
+        let builder = gtk::Builder::new_from_string(glade_src);
 
-        let add_button =
-            gtk::Button::new_from_icon_name("document-new", gtk::IconSize::LargeToolbar);
-        header.add(&add_button);
+        // let header = gtk::HeaderBar::new();
+        let header: gtk::HeaderBar = builder.get_object("header").expect("Couldn't get header!");
+        //header.set_show_close_button(true);
+
+        // let add_button =
+        //     gtk::Button::new_from_icon_name("document-new", gtk::IconSize::LargeToolbar);
+        // header.add(&add_button);
 
         let main_box = gtk::Box::new(Vertical, 0);
 
@@ -41,18 +45,19 @@ impl MainWindow {
         let id = statusbar.get_context_id("Status");
         main_box.add(&statusbar);
 
-        let window = gtk::ApplicationWindow::new(application);
+        let window: gtk::ApplicationWindow = builder
+            .get_object("main-window")
+            .expect("Couldn't get main-window");
+        window.set_application(application);
         window.set_titlebar(&header);
-        window.set_title("YT Video Subscriber");
+        // window.set_title("YT Video Subscriber");
         window.set_default_size(800, 600);
 
         window.add(&main_box);
 
-        add_button.connect_clicked(clone!(statusbar => move |_| {
-            statusbar.push(id, "Import clicked");
-        }));
-
-        // window.show_all();
+        // add_button.connect_clicked(clone!(statusbar => move |_| {
+        //     statusbar.push(id, "Import clicked");
+        // }));
 
         MainWindow {
             context_id: id,
